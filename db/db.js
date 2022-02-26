93f088cff49c4c95842e2a3205d6c1d6a96ba2e8
Boobalan/28 <boobal7722@gmail.com>
2022-02-26 21:32:45 +0530

    First commit

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connect MongoDB at default port 27017.
+const url = "mongodb+srv://Boobalan:boobalan1234@cluster0.ixzoi.mongodb.net/Blog-App";
+let mong = mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
